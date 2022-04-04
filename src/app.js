49a94d080245bbd13bb503d22049d183eb8ac8a4
Adosz <adam.rakonczai97@gmail.com>
2022-04-04 09:22:04 +0200

    feat: starting sequelize implementation

--- src/app.js
+++ src/app.js
@@ -2,30 +2,57 @@ const express = require('express')
 const app = express()
 const routes = require('./routes/routes')
 const cors = require('cors')
-
-console.log('Server starting...')
+// const { Sequelize } = require('sequelize');
+// import { database } from '../config';
 
 try{
-  const bp = require('body-parser')
-  app.use(bp.json())
-  app.use(bp.urlencoded({ extended: true }))
-  console.log("body parser ok");
+
+  console.log('Server starting...')
+
+  try{
+    const bp = require('body-parser')
+    app.use(bp.json())
+    app.use(bp.urlencoded({ extended: true }))
+    console.log("[bodyParser] initialized");
+  }catch(error){
+    console.error("[bodyParser] "+error);
+  }
+
+  try{
+    const corsOptions ={
+        origin:'http://localhost:3000', 
+        methods: [
+            'GET',
+            'POST',
+          ],
+        credentials:true, 
+        optionSuccessStatus:200,
+    }
+    app.use(cors(corsOptions))
+    console.log("[cors] initialized");
+  }catch(error){
+    console.error("[cors] "+error);
+  }
+
+  // try{
+  //   const { dev } = database;
+
+  //   const sequelize = new Sequelize(dev.database, dev.user, dev.password, {
+  //       host: dev.host,
+  //       dialect: dev.driver
+  //   })
+  //   sequelize.authenticate();
+  //   console.log('[sequelize] Connection has been established successfully.');
+  // }catch(error){
+  //   console.error("[sequelize] "+ error)
+  // }
+
+  app.use('/api', routes)
+
+  app.listen(3001, ()=>{
+    console.log("api running on port 3001")
+  })
+  
 }catch(error){
-  console.log("body parser error: "+error);
+    console.error("Error during startring the server");
 }
-
-
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    methods: [
-        'GET',
-        'POST',
-      ],
-    credentials:true, 
-    optionSuccessStatus:200,
-}
-app.use(cors(corsOptions))
-
-app.use('/api', routes)
-
-app.listen(3001)
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,13 +3,22 @@ const router = require('express').Router()
 const users = [{name: "Adam", password: "asdfgh"},{name: "Pete", password: "íyxcvb!!"}]
 
 router.get('/', (req, res) => {
-    console.log('request call: api/users/')
-    res.send(users)
+    try{
+        console.log('request call: api/users/')
+        res.send(users)
+    }catch(error){
+        console.error("request failed: api/users/")
+    }
 })
 
 router.post('/registration', (req, res)=>{
-    users.push(req.body)
-    res.send(users)
+    try{
+        console.log('request call: api/users/registration')
+        users.push(req.body)
+        res.send(users)
+    }catch(error){
+        console.error("request failed: api/users/registration")
+    }
 })
 
 module.exports = router
