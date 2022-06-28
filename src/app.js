e0cbc28d654dae516191f1d0cc1d9dc9b6ce105f
Adosz <adam.rakonczai97@gmail.com>
2022-06-28 21:57:19 +0200

    fix: cors origin update

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ try{
 
   try{
     const corsOptions ={
-        origin:'http://localhost:3000', 
+        origin: ['http://localhost:3000', "https://catering-frontend-staging-2022.herokuapp.com:13132"],
         methods: [
             'GET',
             'POST',
